183bf778d39741ad08726cf12fd8bec1e8f9d861
Mondher bha <mondher.benhajammar@esprit.tn>
2022-02-23 20:10:48 +0100

    help

---
+++ src/Entity/Categorie.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategorieRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CategorieRepository::class)
+ */
+class Categorie
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\OneToOne(targetEntity=equipement::class, cascade={"persist", "remove"})
+     */
+    private $equipement;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getEquipement(): ?equipement
+    {
+        return $this->equipement;
+    }
+
+    public function setEquipement(?equipement $equipement): self
+    {
+        $this->equipement = $equipement;
+
+        return $this;
+    }
+}
